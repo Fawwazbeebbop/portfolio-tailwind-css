abd5410fe77015eae9fafdc1d58b464b83b47de3
Fawwazbeebbop <115068689+Fawwazbeebbop@users.noreply.github.com>
2024-01-19 21:41:17 +0700

    Update Portfolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -54,3 +54,11 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
   darkToggle.checked = false;
 }
+
+//menghilangkan hamburger menu dimana saja
+window.addEventListener('click', function(e){
+  if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
